724d57bb91908d2b90e52e72de34a5e878470231
Luc Pionchon <pionchon.luc@gmail.com>
2013-06-16 09:13:32 +0300

    new: bash completion for package names. (+ cleanup)

--- p-package-manager.sh
+++ p-package-manager.sh
@@ -1,8 +1,8 @@
 #
 #                 The 'p-' package manager
+#                 ========================
 #
-#
-#    /a simple package manager for Debian based systems/
+#     /a simple package manager for Debian based systems/
 #
 #
 #
@@ -29,22 +29,21 @@
 #
 #===============================================================================
 #
-# Usage:
+# Installation and Usage:
 #    - save the file in ~/.bash.d/p-package-manager.sh
 #      if you save it somewhere else, edit the alias p-help (the first grep)
 #    - bash 'source' it
 #    - run p-help
 #
 #===============================================================================
-
-
+#
 #
 # (This is a self documented code: p-help will call grep on this file,
 #                                                         this will be grep-ed:
 #                                                                      #@p-help@
 #      The 'p-' package manager                                        #@p-help@
 #      ------------------------                                        #@p-help@
-#      version 0.5                                                     #@p-help@
+#      version 0.6                                                     #@p-help@
 #                                                                      #@p-xxx-help@
 #   p- is a wrapper to Debian package management.                      #@p-xxx-help@
 #   p- unifies calls to dpkg, aptitude, apt-get, etc.                  #@p-xxx-help@
@@ -52,30 +51,22 @@
 #   p- commands are all 'p-' prefixed.                                 #@p-xxx-help@
 #   p- commands are just Shell aliases,                                #@p-xxx-help@
 #      --> use all the original panameters.                            #@p-xxx-help@
-#                                                                      #@p-xxx-help@
-#  Dependencies:                                                       #@p-xxx-help@
-#  - dpkg, apt-get, aptitude,                                          #@p-xxx-help@
-#  - dch, dpkg-source, dpkg-buildpackage                               #@p-xxx-help@
-#  - locale-purge                                                      #@p-xxx-help@
-#  - bash, grep, sed, column, less, sudo                               #@p-xxx-help@
-#                                                                      #@p-xxx-help@
-# FIXME : why bash completion does not work?                           #@p-xxx-help@
 # 
 
 alias p-help='\grep "#@p-help@" ~/.bash.d/p-package-manager.sh   |   \grep --invert-match "alias p-help="   |   \sed -e "s/#@p-help@//; s/^alias /   /; s/^#---- //; s/^#//; s/ *$//; s/=\"/##/; s/\"//;"   |   \column -s "##" -t -e   |   \less --no-init --quit-if-one-screen   '    ##############
 #---- What the pipeline does:
-# grep all #+@p-help@ tagged lines in this file
-# grep except this 'p-help' line
-# sed removes #+@p-help@ tags
-# sed replaces all 'alias ' by small indentation
-# sed removed all '#---- ' to left align headings
-# sed removes all starting '#'
-# sed removes all trailing white spaces
-# sed removes all '="'
-# sed removes all remaining '"'
+#  grep all #@p-help@ tagged lines in this file
+#       except this 'p-help' line
+#  sed removes #+@p-help@ tags
+#      replaces all 'alias ' by small indentation
+#      removes all '#---- ' to left align headings
+#      removes all starting '#'
+#      removes all trailing white spaces
+#      removes all '="'
+#      removes all remaining '"'
 
-_p_version () { dpkg --status $1 | \grep "Version:"; }
-_p_search_description () { aptitude search "?description($1)"; }
+_dpkg_version       () { dpkg --status $1 | \grep "Version:"; }
+_aptit_search_descr () { aptitude search "?description($1)"; }
 #                                                                      #@p-help@
 #   <p-command>   =" <deb command>                  ## <min args>      #@p-help@
 #   ------------- =" ---------------                ## ------------    #@p-help@
@@ -83,13 +74,13 @@ _p_search_description () { aptitude search "?description($1)"; }
 #---- Query installed packages:                                        #@p-help@
 alias p-status="dpkg --status"                      ## + package name  #@p-help@
 alias p-list-files="dpkg --listfiles"               ## + package name  #@p-help@
-alias p-version="_p_version"                        ## + package name  #@p-help@
+alias p-version="_dpkg_version"                     ## + package name  #@p-help@
 alias p-owning-file="dpkg -S"                       ## + filename      #@p-help@
 #                                                                      #@p-help@
 #---- Search for remote packages:                                      #@p-help@
 alias p-search="apt-cache search"                   ## + keywords      #@p-help@
 alias p-search-name="aptitude search"               ## + keywords      #@p-help@
-alias p-search-description="_p_search_description"  ## + keywords      #@p-help@
+alias p-search-description="_aptit_search_descr"    ## + keywords      #@p-help@
 #                                                                      #@p-help@
 #---- Install packages, remote or local:                               #@p-help@
 alias p-install="sudo aptitude install"             ## + package list  #@p-help@
@@ -112,3 +103,19 @@ alias p-src-commit-patch="dpkg-source --commit"     ## (no arg)        #@p-help@
 alias p-src-build-package="dpkg-buildpackage"       ## (no arg)        #@p-help@
 #                                                                      #@p-help@
 #-------------------------------------------------------------------------------
+#
+# Bash completion
+#
+complete -F _pkg_names p-status
+complete -F _pkg_names p-list-files
+complete -F _pkg_names p-version
+
+complete -F _pkg_names p-install
+complete -F _pkg_names p-remove
+
+complete -F _pkg_names p-src-get-source
+complete -F _pkg_names p-src-get-build-dep
+
+
+
+echo "p- done."
