f727383a475f689baa2374cc5547ff4356523f71
Luc Pionchon <pionchon.luc@gmail.com>
2016-11-14 13:18:58 +0100

    0.9pre

--- p-package-manager.sh
+++ p-package-manager.sh
@@ -43,7 +43,7 @@
 #                                                                      #@p-help@
 #      The 'p-' package manager                                        #@p-help@
 #      ------------------------                                        #@p-help@
-#      version 0.8                                                     #@p-help@
+#      version 0.9pre                                                  #@p-help@
 #                                                                      #@p-xxx-help@
 #   p- is a wrapper to Debian package management.                      #@p-xxx-help@
 #   p- unifies calls to dpkg, aptitude, apt-get, etc.                  #@p-xxx-help@
